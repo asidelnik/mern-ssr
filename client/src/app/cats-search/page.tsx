f86b1dc68cbf45bc9b22461b1205f23f3988a96f
Amos <asidelnik@gmail.com>
2024-03-11 10:44:52 +0200

    Client - fix CatsSearch page useSearchParams suspense error. Fixed build & start (much faster render).

--- client/src/app/cats-search/page.tsx
+++ client/src/app/cats-search/page.tsx
@@ -1,52 +1,13 @@
 'use client'
-import c from './CatsSearchPage.module.scss';
-import CatSmallCard from '@/components/cat-small-card/CatSmallCard';
-import { baseUrl, serverPaths } from '@/constants/api';
-import { CatType } from '@/types/CatType';
-import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { Suspense } from 'react';
+import CatsSearchSuspensed from './CatsSearchSuspensed';
 
 export default function CatsSearch() {
-  const [cats, setCats] = useState<CatType[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-
-  const searchParams = useSearchParams();
-  const catName = searchParams.get('name');
-  // Use catName as a query param in a get request
-  useEffect(() => {
-    if (catName) {
-      fetchData(catName);
-    }
-  }, [catName]);
-
-  const fetchData = async (name: string) => {
-    setIsLoading(true);
-    try {
-      const path = `${baseUrl}${serverPaths.catSearch}?name=${name}`;
-      const response = await fetch(path);
-      const data: CatType[] = await response.json();
-      setCats(data);
-    } catch (err: unknown) {
-      if (err instanceof TypeError) {
-        // Handle fetch TypeError
-        setError((err as TypeError).message);
-      } else {
-        // Handle generic errors
-        setError((err as Error).message);
-      }
-    } finally {
-      setIsLoading(false);
-    }
-  }
-
   return (
     <>
-      <div className={c.cardsContainer}>
-        {cats.map((cat: CatType) => (
-          <CatSmallCard key={cat._id} {...cat} />
-        ))}
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <CatsSearchSuspensed />
+      </Suspense>
     </>
   )
 }
