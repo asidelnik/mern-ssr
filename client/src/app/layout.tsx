5c2a03e2bd6f5252e5875bc9a74d715084afeeca
Amos <asidelnik@gmail.com>
2024-03-13 17:08:37 +0200

    Client - favicon added

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={c.body + ' ' + inter.className}>
         <header className={c.header}>
           <div className={c.left}>
