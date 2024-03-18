15940113f22d48aad2f9daeb0b39a5cb7d3dc6da
Amos <asidelnik@gmail.com>
2024-03-18 20:31:41 +0200

    API consts & methods

---
+++ client-remix/app/constants/api.ts
@@ -0,0 +1,9 @@
+export const baseUrl = 'http://localhost:5050';
+
+export const serverPaths = {
+  cats: '/cats/',
+  topRated: '/cats/top-rated',
+  otherCats: '/cats/other-cats',
+  search: '/cats/search/',
+  likeCat: '/cats/add-like',
+};
---
+++ client-remix/app/requests/requests.ts
@@ -0,0 +1,40 @@
+import { baseUrl, serverPaths } from '~/constants/api';
+
+export async function getTopRatedCats<T>(): Promise<T> {
+  const path = `${baseUrl}${serverPaths.topRated}`;
+  const res = await fetch(path, { cache: 'no-cache' });
+  if (!res.ok) {
+    throw new Error('Please try later');
+  }
+  return res.json() as Promise<T>;
+}
+
+export async function getCatsByName<T>(name: string | null): Promise<T> {
+  // if (name === null) {
+  //   throw new Error('No cat searched');
+  // }
+  const path = `${baseUrl}${serverPaths.search}?name=${name}`;
+  const res = await fetch(path, { cache: 'no-cache' });
+  if (!res.ok) {
+    throw new Error('Please try later');
+  }
+  return res.json() as Promise<T>;
+}
+
+export async function getCatById<T>(id: string): Promise<T> {
+  const path = `${baseUrl}${serverPaths.cats}/${id}`;
+  const res = await fetch(path, { cache: 'no-cache' });
+  // if (!res.ok) {
+  //   notFound();
+  // }
+  return res.json() as Promise<T>;
+}
+
+export async function likeCat<T>(id: string): Promise<T> {
+  const path = `${baseUrl}${serverPaths.likeCat}/${id}`;
+  const res = await fetch(path, { method: 'PUT' });
+  if (!res.ok) {
+    throw new Error('Please try later');
+  }
+  return res.json() as Promise<T>;
+}
