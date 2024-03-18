2b54bfb5608b20379040e6e62a02255ebf9e200c
Amos <asidelnik@gmail.com>
2024-03-18 20:31:16 +0200

    Types

---
+++ client-remix/app/types/CatType.ts
@@ -0,0 +1,25 @@
+export type CatType = {
+  _id: string;
+  name: string;
+  birthday: string; // ISODate is not a valid TypeScript type, use string instead
+  age: number;
+  address: Address;
+  favoriteFood: string;
+  colors: string[];
+  heightCM: number;
+  weightG: number;
+  image: string;
+  likeCount: number;
+  breed: string;
+};
+
+type Address = {
+  street: string;
+  city: string;
+  country: string;
+  zipCode: string;
+};
+
+export type CatCardProps = CatType & {
+  fetchPriority?: 'high' | 'low' | 'auto';
+};
